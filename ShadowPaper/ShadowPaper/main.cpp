524d34cf9591a73d4a83a9783951a6276c61d271
TeamProjectTCA <34767044+TeamProjectTCA@users.noreply.github.com>
2018-02-26 10:33:25 +0900

    Add files via upload

---
+++ ShadowPaper/ShadowPaper/main.cpp
@@ -0,0 +1,29 @@
+#include "DxLib.h"
+
+int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow ) {
+	SetWindowText( "ShadowPaper" );
+	ChangeWindowMode( 1 );
+	//SetWindowSize( SCREEN_WIDTH, SCREEN_HEIGHT );
+	//SetGraphMode( WIDTH, HEIGHT, 32 );
+	SetDoubleStartValidFlag( TRUE );
+	SetAlwaysRunFlag( TRUE );
+	//SetConnectTimeOutWait( 60 );
+	if ( DxLib_Init( ) != 0 ) {
+		return -1;
+	}
+	SetDrawScreen( DX_SCREEN_BACK );
+
+	while ( true ) {
+		if ( ScreenFlip( ) != 0 || ProcessMessage( ) != 0 || ClearDrawScreen( ) != 0 ) {
+			break;
+		}
+
+		if ( CheckHitKey( KEY_INPUT_ESCAPE ) ) {
+			break;
+		}
+	}
+
+
+	DxLib_End( );
+	return 0;
+}
